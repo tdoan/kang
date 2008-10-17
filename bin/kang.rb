b132e61a5ae4a99d928250bfc389456e3c2393d9
Tony Doan <tdoan@tdoan.com>
2008-10-17 14:32:21 -0700

    Adding README, COPYING and notices about COPYING into kang.rb.

--- bin/kang.rb
+++ bin/kang.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
-#KANG - The Ruby Regexp Debugger
-# Copyright
-# Tony Doan <tdoan@tdoan.com>
-# 2008-10-16
+#
+# kang.rb - Main executable for GUI interface
+#
+# ====================================================================
+# Copyright (c) 2008 Tony Doan <tdoan@tdoan.com>.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://github.com/tdoan/kang/tree/master/COPYING.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+# ====================================================================
+#
+
 require 'rubygems'
 require 'wx'
 include Wx
