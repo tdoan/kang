ab2733b2b4b55368c02ab31128d15f27546559f5
Tony Doan <tdoan@tdoan.com>
2008-11-06 14:16:57 -0800

    Move highlighting based on text field changes into a method. Make necessary object points into instance variables. Call highlighting code now for both text and text2.

--- bin/kang.rb
+++ bin/kang.rb
@@ -28,59 +28,62 @@ class EventFrame < Wx::Frame
     m = Wx::MessageDialog.new(self, text, title, Wx::OK | Wx::ICON_INFORMATION)
     m.show_modal()
   end
-  
+
   def initialize()
-    highlight = Wx::TextAttr.new(Wx::GREEN, Wx::Colour.new(255, 255, 0) )
-    normal    = Wx::TextAttr.new(Wx::BLACK, Wx::WHITE) #, Wx::Colour.new(255, 255, 0) )
-    
+    @highlight = Wx::TextAttr.new(Wx::GREEN, Wx::Colour.new(255, 255, 0) )
+    @normal    = Wx::TextAttr.new(Wx::BLACK, Wx::WHITE) #, Wx::Colour.new(255, 255, 0) )
+
     super(nil, -1, "Event Frame")
     set_client_size(Wx::Size.new(640,480))
     sizer = Wx::BoxSizer.new(Wx::VERTICAL)
-    text  = Wx::TextCtrl.new(self,-1,'Regex in here',Wx::DEFAULT_POSITION,Wx::DEFAULT_SIZE,Wx::TE_MULTILINE|TE_RICH)
-    text2 = Wx::TextCtrl.new(self,-1,'Text in here',Wx::DEFAULT_POSITION,Wx::DEFAULT_SIZE,Wx::TE_MULTILINE)
-    sizer.add(text, 1,Wx::GROW|Wx::ALL,2)
-    sizer.add(text2,1,Wx::GROW|Wx::ALL,2)
+    @text  = Wx::TextCtrl.new(self,-1,'Regex in here',Wx::DEFAULT_POSITION,Wx::DEFAULT_SIZE,Wx::TE_MULTILINE|TE_RICH)
+    @text2 = Wx::TextCtrl.new(self,-1,'Text in here',Wx::DEFAULT_POSITION,Wx::DEFAULT_SIZE,Wx::TE_MULTILINE)
+    sizer.add(@text, 1,Wx::GROW|Wx::ALL,2)
+    sizer.add(@text2,1,Wx::GROW|Wx::ALL,2)
     @status = StatusBar.new(self,-1)
     sizer.add(@status,0,Wx::ALIGN_LEFT,2)
     self.set_sizer(sizer)
-    evt_text(text.get_id) do | event |
-      size = text2.get_value.size
-      begin 
-        r = Regexp.new(text.get_value)
-      rescue
-      end
-      unless r.nil?
-        puts "Valid Regex!"
-        md = r.match(text2.get_value)
-        unless md.nil?
-          b = md.begin(0)
-          e = md.end(0)
-          puts b
-          puts e
-          puts text2.get_value[b..e-1]
-          ret=text2.set_style(0,size,normal)
-          text2.append_text("")
-          text2.set_style(b,e,highlight)
-          text2.append_text("")
-          puts ret
-        else
-          ret=text2.set_style(0,size,normal)
-          text2.append_text("")
-        end
-      else
-        ret=text2.set_style(0,size,normal)
-        text2.append_text("")
-      end
-    end
+    evt_text(@text.get_id){|event| text_change(event)}
+    evt_text(@text2.get_id){|event| text_change(event)}
   end
 
   def set_status(text)
     @status.pushd_status_text(text)
   end
+
+  def text_change(event)   
+    size = @text2.get_value.size
+    begin 
+      r = Regexp.new(@text.get_value)
+    rescue
+    end
+    unless r.nil?
+      puts "Valid Regex!"
+      md = r.match(@text2.get_value)
+      unless md.nil?
+        b = md.begin(0)
+        e = md.end(0)
+        puts b
+        puts e
+        puts @text2.get_value[b..e-1]
+        ret= @text2.set_style(0,size,@normal)
+        @text2.append_text("")
+        @text2.set_style(b,e,@highlight)
+        @text2.append_text("")
+        puts ret
+      else
+        ret = @text2.set_style(0,size,@normal)
+        @text2.append_text("")
+      end
+    else
+      ret = @text2.set_style(0,size,@normal)
+      @text2.append_text("")
+    end
+  end
 end
 
 class Kang < App
-  
+
   def on_init
     EventFrame.new.show
   end
