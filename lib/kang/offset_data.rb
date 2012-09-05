cab24942d89ee9ad73e49f6bead032ea669db996
Tony Doan <tdoan@tdoan.com>
2012-09-04 20:10:50 -0700

    Updating copyrights and cleaning out some cruft.

--- lib/kang/offset_data.rb
+++ lib/kang/offset_data.rb
@@ -1,3 +1,17 @@
+#
+# offset_data.rb - Main executable for GUI interface
+#
+# ====================================================================
+# Copyright (c) 2012 Tony Doan <tdoan@tdoan.com>.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://github.com/tdoan/kang/tree/master/COPYING.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+# ====================================================================
+#
+
 module Kang
   class OffSetMatchData
     def initialize(offset,match_data)
